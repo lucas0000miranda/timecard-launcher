4cf87b97cddd5a029998a16a3b446151cde2bde9
Lucas Miranda <lmiranda@2mundos.net>
2021-04-28 03:44:25 -0300

    TASK-001: add test to backend

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
 require __DIR__."/vendor/autoload.php";
+require_once('./database/config.php');
+require_once('./database/Connection.php');
+
+$connection = new Connection(HOST, USER, PASSWORD, DB);
+
+$test = $connection->getRow('select * from users');
+
+var_dump(json_encode($test));
+
+
